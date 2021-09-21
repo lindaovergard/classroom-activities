3c5817a6fbc38347e1804513fd7fca179641d609
lovergard76 <83507205+lovergard76@users.noreply.github.com>
2021-09-20 21:36:09 -0400

    19.1-activities

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,20 @@
 // Create a variable below
+var helloWorld='Hello World!';
+
+var linda='Jeg er Linda!';
+
 
 // Console.log() your variable here.
+console.log(helloWorld);
+console.log(linda);
+
+
+
+
+
+var redhat=5;
+var bluehat=9;
+
+var total='Your Total is' + (redhat+bluehat);
+
+alert(total);
--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,30 @@
 // Create the addNumbers function here.
+function addNumbers() {
+    var two=2;
+    var three=3;
+    var five = two + three;
 
-// Create the stringConcat function here.
+    console.log(five);
+    
+    
+}
+
+function stringConcat() {
+    var taco = 'taco';
+    var cat= 'cat';
+    var tacoCat = 'tacocat' + (taco + cat);
+
+    console.log(tacoCat);
+    
+    
+}
+
+
+
+addNumbers();
+
+
+
+stringConcat();
+
+alert(tacoCat);
